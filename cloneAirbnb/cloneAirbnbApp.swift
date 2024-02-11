f7e44088883d47f9a7f5b2505de315af92646476
andrishe <115075619+andrishe@users.noreply.github.com>
2024-02-11 16:54:04 +0100

    Initial Commit

---
+++ cloneAirbnb/cloneAirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloneAirbnbApp.swift
+//  cloneAirbnb
+//
+//  Created by Andri on 11/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct cloneAirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
